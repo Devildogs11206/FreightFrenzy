4b107ee415f4078f43a9b09a4a9cb05c8267202d
Devildogs11206 <devildogs11206@gmail.com>
2022-01-09 16:36:23 -0600

    added Highgoal midgoal and lowgoal presets

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controllers/LiftController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controllers/LiftController.java
@@ -3,6 +3,9 @@ package org.firstinspires.ftc.teamcode.controllers;
 import static org.firstinspires.ftc.teamcode.internal.Robot.LiftMode.BACKWARD;
 import static org.firstinspires.ftc.teamcode.internal.Robot.LiftMode.FORWARD;
 import static org.firstinspires.ftc.teamcode.internal.Robot.LiftMode.STOPPED;
+import static org.firstinspires.ftc.teamcode.internal.Robot.LiftPosition.HIGHGOAL;
+import static org.firstinspires.ftc.teamcode.internal.Robot.LiftPosition.LOWGOAL;
+import static org.firstinspires.ftc.teamcode.internal.Robot.LiftPosition.MIDGOAL;
 
 import org.firstinspires.ftc.teamcode.internal.Robot;
 import org.firstinspires.ftc.teamcode.opmodes.OpMode;
@@ -17,6 +20,9 @@ public class LiftController extends RobotController {
         if (gamepad2.y) robot.lift(Robot.LiftPosition.CAROUSEL);
         else if (gamepad2.right_bumper) robot.lift(FORWARD);
         else if (gamepad2.left_bumper) robot.lift(BACKWARD);
+        else if (gamepad2.dpad_up) robot.lift(HIGHGOAL);
+        else if (gamepad2.dpad_right || gamepad2.dpad_left) robot.lift(MIDGOAL);
+        else if (gamepad2.dpad_down) robot.lift(LOWGOAL);
         else robot.lift(STOPPED);
     }
 }
