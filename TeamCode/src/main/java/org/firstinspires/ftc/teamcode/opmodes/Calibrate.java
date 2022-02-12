8844c188f6d7581516ec7637a56c769caef779a2
Devildogs11206 <devildogs11206@gmail.com>
2022-02-11 20:04:20 -0600

    Revert "Removing Calibrate"
    This reverts commit 4f04022743303ba618692cea387aecb93f6f9a1e.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Calibrate.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import static org.firstinspires.ftc.teamcode.internal.Robot.LiftPosition.HIGHGOAL;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous
+public class Calibrate extends OpMode {
+    @Override
+    protected void execute() {
+       robot.calibrate();
+    }
+}
